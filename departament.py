0d101c540e037691f9df717f42fd7359aaa70ef6
ghellardev <r.ghelbere@gmail.com>
2023-02-14 10:50:21 +0200

    Am introdus doua metode de clasa una penru validare input float iar una pentru validare data

--- departament.py
+++ departament.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Departament:
     # Variabila de clasa care va contine obiecte de tip Departament
     lista_departamente = []
@@ -5,3 +8,21 @@ class Departament:
     def __init__(self, workdep):
         """ Constructorul clasei Departament. """
         self.workdep = workdep
+
+    # Metoda folosita pentru a introduce o valoare float valida
+    @classmethod
+    def float_input(cls):
+        try:
+            return float(input())
+        except ValueError:
+            print("Introduceti un numar:")
+            return Departament.float_input()
+
+    # Metoda folosita pentru a valida data introdusa
+    @classmethod
+    def data_input(cls, data):
+        try:
+            return datetime.strptime(data, "%d-%m-%Y").date()
+        except ValueError:
+            print("Data nu este valida!")
+            return None
