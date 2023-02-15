b17c4874c9e598683956ac7d663aecadee8f3d98
ghellardev <r.ghelbere@gmail.com>
2023-02-15 06:55:13 +0200

    Impartit functia alegere_dep in creare_dict_dep si alegere_dep creat o metoda de a afisa pe baza obiectului un singur departament si am bazat metoda de vizualizare a tuturor dep pe aceasta

--- departament.py
+++ departament.py
@@ -6,6 +6,9 @@ class Departament:
     # Variabila de clasa care va contine denumirile departamentelor deja existente in sistem
     lista_departamente = []
 
+    # Variabila de clasa care va contine maparea intre "indexul unui departament si denumirea acestuia"
+    dict_optiuni = {}
+
     @classmethod
     def departamente(cls):
         cursor = Date_Angajati.find({}, {"_id": 0, "Departament": 1})
@@ -15,18 +18,23 @@ class Departament:
         #     print(dep)
 
     @classmethod
-    def alegere_dep(cls):
-        dict_optiuni = {}
+    def creare_dict_dep(cls):
+        Departament.departamente()
         counter = 0
         for i in Departament.lista_departamente:
             counter += 1
-            dict_optiuni[str(counter)] = i
-        for key, val in dict_optiuni.items():
-            print(f"{key}. {val}")
+            Departament.dict_optiuni[str(counter)] = i
+
+    @classmethod
+    def alegere_dep(cls):
+        Departament.creare_dict_dep()
         while True:
+            for key, val in Departament.dict_optiuni.items():
+                print(f"{key}. {val}")
+            print("  Alegeti un dep: ")
             optiune = input()
-            if optiune in dict_optiuni:
-                return dict_optiuni[optiune]
+            if optiune in Departament.dict_optiuni:
+                return Departament.dict_optiuni[optiune]
             else:
                 print("Nu ati introdus o optiune valida.")
 
@@ -54,9 +62,18 @@ class Departament:
 
     @classmethod
     def find_all(cls):
+        Departament.creare_dict_dep()
+        for dep in Departament.dict_optiuni.values():
+            Departament(dep).find_all_in_dep()
+        input("Press return to continue...")
+
+    def find_all_in_dep(self):
+        Departament.creare_dict_dep()
         print(35 * "=")
-        for i in Date_Angajati.find({}, {"_id": 0}):
+        print(self.workdep.center(35))
+        print(35 * "=")
+        for i in Date_Angajati.find({"Departament": self.workdep}, {"_id": 0}):
             for attr, value in i.items():
                 print(f"{attr}: {value}")
             print(35 * "=")
-        input("Press return to continue...")
+        # input("Press return to continue...")
